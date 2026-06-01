3fae1d8154d9b73b9e35e7f302f6d99afc5dcf2f
mijaelmanuel <mijaelmg03@gmail.com>
2026-06-01 10:12:08 -0500

    Add files via upload

---
+++ OmitirMultiplos.java
@@ -0,0 +1,13 @@
+public class OmitirMultiplos {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i % 5 == 0) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+    }
+}
